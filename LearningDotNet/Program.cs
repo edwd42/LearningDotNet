f108d5df1436cbd71932d0f5a853fa18f0b225c2
edward.dupre <edward.dupre@redcross.org>
2021-09-04 19:45:58 -0400

    add BitArray enemyGrid

--- LearningDotNet/Program.cs
+++ LearningDotNet/Program.cs
@@ -17,51 +17,51 @@ namespace LearningDotNet
             customers.Add("You");
             customers.Add("Them");
 
-            Console.WriteLine(customers.Count);
+            Console.WriteLine("Customers count = " + customers.Count);
 
             foreach (var customer in customers)
             {
-                Console.WriteLine(customer);
+                Console.WriteLine("customer = " + customer);
             }
 
-            Console.WriteLine(customers[1]);
+            Console.WriteLine("customers[1] = " + customers[1]);
 
             Dictionary<String, String> config = new Dictionary<string, string>();
             config.Add("resolution", "1920x1080");
             config.Add("title", "MyWebsite");
 
-            Console.WriteLine(config["title"]);
+            Console.WriteLine("config[title] = " + config["title"]);
 
             foreach (var setting in config)
             {
-                Console.WriteLine(setting.Value);
+                Console.WriteLine("setting.Value = " + setting.Value);
             }
 
             // boxing
             int i = 123;
             object o = i;
 
-            Console.WriteLine(i);
-            Console.WriteLine(o);
+            Console.WriteLine("boxed i = " + i);
+            Console.WriteLine("boxed o = " + o);
 
             // unboxing
             i = (int)o;
 
-            Console.WriteLine(i);
-            Console.WriteLine(o);
+            Console.WriteLine("unboxed i = " + i);
+            Console.WriteLine("unboxed o = " + o);
 
             // ArrayList is depricated
             ArrayList list = new ArrayList();
             list.Add("some string");
             String s = (String)list[0];
 
-            Console.WriteLine(list[0]);
+            Console.WriteLine("list[0] = " + list[0]);
 
             // Hashtables
             Hashtable table = new Hashtable();
             table.Add("title", "My Hashtable");
             var t = (String)table["title"];
-            Console.WriteLine(t);
+            Console.WriteLine("My Hashtable[title] = " + t);
 
             // Concurrent for thread-safe collections
             Thread thread1 = new Thread(new ThreadStart(AddItem));
@@ -69,12 +69,25 @@ namespace LearningDotNet
 
             thread1.Start();
             thread2.Start();
+
+            // BitArray
+            bool[] preload = new bool[3] { true, false, true };
+            BitArray enemyGrid = new BitArray(preload);
+            //enemyGrid[0] = false;
+            //enemyGrid[1] = true;
+            //enemyGrid.Set(2, false);
+
+            foreach (var item in enemyGrid)
+            {
+                Console.WriteLine("BitArray.item = " + item);
+            }
+
         }
 
         static void AddItem()
         {
             items.TryAdd(1, 2);
-            Console.WriteLine(items.Count);
+            Console.WriteLine("items.Count = " + items.Count);
             //Console.WriteLine(items[0]);
             //Console.WriteLine(items[1]);
         }
